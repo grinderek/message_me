f81a4c4f6225cf1ce454bb27c1efd5981927a9fd
Danila Berazhny <dannmmaster@gmail.com>
2021-10-29 17:52:45 +0300

    Create User and Message models

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+
 const images = require.context('../images', true)
 
 $(document).on('turbolinks:load', function () {
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "evgen", password: "password")
+User.create(username: "man", password: "password")
+User.create(username: "mac", password: "password")
+User.create(username: "love", password: "password")
+User.create(username: "haruki", password: "password")
+
+Message.create(body: "Hi", user_id: 1)
+Message.create(body: "Hi, man", user_id: 3)
+Message.create(body: "Hello", user_id: 2)
+Message.create(body: "How are you?", user_id: 4)
+Message.create(body: "Ok", user_id: 2)
+Message.create(body: "Nice", user_id: 5)
